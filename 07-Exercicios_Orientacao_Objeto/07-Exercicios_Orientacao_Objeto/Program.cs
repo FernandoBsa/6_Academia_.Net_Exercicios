8d983b088121d5770470b67baf05a0a7e9967639
Fernando Barros <fernandobarrosdesak@gmail.com>
2023-10-26 15:02:14 -0300

    Resolucao dos exercicio_07

---
+++ 07-Exercicios_Orientacao_Objeto/07-Exercicios_Orientacao_Objeto/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _07_Exercicios_Orientacao_Objeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.CadastrarPessoa("Fernando Barros", 28, "00011122269", "1234567");
+            pessoa.ApresentarPessoa();
+        }
+    }
+}
